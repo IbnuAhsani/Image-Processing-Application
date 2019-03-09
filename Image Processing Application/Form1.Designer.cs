7107210bb458bfaf92968bbbac33508cd0a2e0af
Ibnu Ahsani <ibnu.muhari@gmail.com>
2019-03-09 11:39:04 +0700

    change PictureBox Image feature

---
+++ Image Processing Application/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Image_Processing_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.changePictureButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(12, 58);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(431, 344);
+            this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // changePictureButton
+            // 
+            this.changePictureButton.Location = new System.Drawing.Point(487, 357);
+            this.changePictureButton.Name = "changePictureButton";
+            this.changePictureButton.Size = new System.Drawing.Size(100, 45);
+            this.changePictureButton.TabIndex = 1;
+            this.changePictureButton.Text = "Ganti Gambar";
+            this.changePictureButton.UseVisualStyleBackColor = true;
+            this.changePictureButton.Click += new System.EventHandler(this.changePictureButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.changePictureButton);
+            this.Controls.Add(this.mainPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.Button changePictureButton;
+    }
+}
+
---
+++ Image Processing Application/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Processing_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void changePictureButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.bmp)|*.jpg; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                mainPictureBox.Image = new Bitmap(open.FileName);
+            }
+        }
+    }
+}
