6cb2ac8cf5b457a9cf62c1477be8df0486739d14
Ibnu Ahsani <ibnu.muhari@gmail.com>
2019-03-10 18:47:10 +0700

    add widgets to threshold the image

--- Image Processing Application/Form1.Designer.cs
+++ Image Processing Application/Form1.Designer.cs
@@ -51,13 +51,17 @@
             this.resultPictureLabel = new System.Windows.Forms.Label();
             this.invertPictureButton = new System.Windows.Forms.Button();
             this.greyscaleAveragingButton = new System.Windows.Forms.Button();
+            this.processLabel = new System.Windows.Forms.Label();
+            this.thresholdLabel = new System.Windows.Forms.Label();
+            this.thresholdValueTextBox = new System.Windows.Forms.TextBox();
+            this.thresholdPictureButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // mainPictureBox
             // 
-            this.mainPictureBox.Location = new System.Drawing.Point(102, 44);
+            this.mainPictureBox.Location = new System.Drawing.Point(33, 34);
             this.mainPictureBox.Name = "mainPictureBox";
             this.mainPictureBox.Size = new System.Drawing.Size(224, 156);
             this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -66,7 +70,7 @@
             // 
             // changePictureButton
             // 
-            this.changePictureButton.Location = new System.Drawing.Point(389, 357);
+            this.changePictureButton.Location = new System.Drawing.Point(95, 393);
             this.changePictureButton.Name = "changePictureButton";
             this.changePictureButton.Size = new System.Drawing.Size(100, 45);
             this.changePictureButton.TabIndex = 1;
@@ -76,22 +80,22 @@
             // 
             // xCoordinateTextBox
             // 
-            this.xCoordinateTextBox.Location = new System.Drawing.Point(540, 82);
+            this.xCoordinateTextBox.Location = new System.Drawing.Point(544, 190);
             this.xCoordinateTextBox.Name = "xCoordinateTextBox";
-            this.xCoordinateTextBox.Size = new System.Drawing.Size(79, 20);
+            this.xCoordinateTextBox.Size = new System.Drawing.Size(42, 20);
             this.xCoordinateTextBox.TabIndex = 2;
             // 
             // yCoordinateTextBox
             // 
-            this.yCoordinateTextBox.Location = new System.Drawing.Point(670, 82);
+            this.yCoordinateTextBox.Location = new System.Drawing.Point(627, 190);
             this.yCoordinateTextBox.Name = "yCoordinateTextBox";
-            this.yCoordinateTextBox.Size = new System.Drawing.Size(79, 20);
+            this.yCoordinateTextBox.Size = new System.Drawing.Size(42, 20);
             this.yCoordinateTextBox.TabIndex = 3;
             // 
             // xCoordinateLabel
             // 
             this.xCoordinateLabel.AutoSize = true;
-            this.xCoordinateLabel.Location = new System.Drawing.Point(573, 58);
+            this.xCoordinateLabel.Location = new System.Drawing.Point(557, 163);
             this.xCoordinateLabel.Name = "xCoordinateLabel";
             this.xCoordinateLabel.Size = new System.Drawing.Size(14, 13);
             this.xCoordinateLabel.TabIndex = 4;
@@ -100,7 +104,7 @@
             // yCoordinateLabel
             // 
             this.yCoordinateLabel.AutoSize = true;
-            this.yCoordinateLabel.Location = new System.Drawing.Point(707, 58);
+            this.yCoordinateLabel.Location = new System.Drawing.Point(643, 163);
             this.yCoordinateLabel.Name = "yCoordinateLabel";
             this.yCoordinateLabel.Size = new System.Drawing.Size(14, 13);
             this.yCoordinateLabel.TabIndex = 5;
@@ -109,7 +113,7 @@
             // coordinateLabel
             // 
             this.coordinateLabel.AutoSize = true;
-            this.coordinateLabel.Location = new System.Drawing.Point(461, 85);
+            this.coordinateLabel.Location = new System.Drawing.Point(465, 190);
             this.coordinateLabel.Name = "coordinateLabel";
             this.coordinateLabel.Size = new System.Drawing.Size(52, 13);
             this.coordinateLabel.TabIndex = 6;
@@ -117,7 +121,7 @@
             // 
             // getCoordinatesRGBValueButton
             // 
-            this.getCoordinatesRGBValueButton.Location = new System.Drawing.Point(646, 357);
+            this.getCoordinatesRGBValueButton.Location = new System.Drawing.Point(650, 300);
             this.getCoordinatesRGBValueButton.Name = "getCoordinatesRGBValueButton";
             this.getCoordinatesRGBValueButton.Size = new System.Drawing.Size(103, 45);
             this.getCoordinatesRGBValueButton.TabIndex = 7;
@@ -127,21 +131,21 @@
             // 
             // rValueTextBox
             // 
-            this.rValueTextBox.Location = new System.Drawing.Point(540, 149);
+            this.rValueTextBox.Location = new System.Drawing.Point(544, 253);
             this.rValueTextBox.Name = "rValueTextBox";
             this.rValueTextBox.Size = new System.Drawing.Size(42, 20);
             this.rValueTextBox.TabIndex = 8;
             // 
             // gValueTextBox
             // 
-            this.gValueTextBox.Location = new System.Drawing.Point(623, 149);
+            this.gValueTextBox.Location = new System.Drawing.Point(627, 253);
             this.gValueTextBox.Name = "gValueTextBox";
             this.gValueTextBox.Size = new System.Drawing.Size(42, 20);
             this.gValueTextBox.TabIndex = 9;
             // 
             // bValueTextBox
             // 
-            this.bValueTextBox.Location = new System.Drawing.Point(707, 149);
+            this.bValueTextBox.Location = new System.Drawing.Point(711, 253);
             this.bValueTextBox.Name = "bValueTextBox";
             this.bValueTextBox.Size = new System.Drawing.Size(42, 20);
             this.bValueTextBox.TabIndex = 10;
@@ -149,7 +153,7 @@
             // intensityLabel
             // 
             this.intensityLabel.AutoSize = true;
-            this.intensityLabel.Location = new System.Drawing.Point(461, 156);
+            this.intensityLabel.Location = new System.Drawing.Point(465, 260);
             this.intensityLabel.Name = "intensityLabel";
             this.intensityLabel.Size = new System.Drawing.Size(52, 13);
             this.intensityLabel.TabIndex = 11;
@@ -158,7 +162,7 @@
             // rValueLabel
             // 
             this.rValueLabel.AutoSize = true;
-            this.rValueLabel.Location = new System.Drawing.Point(553, 123);
+            this.rValueLabel.Location = new System.Drawing.Point(557, 227);
             this.rValueLabel.Name = "rValueLabel";
             this.rValueLabel.Size = new System.Drawing.Size(15, 13);
             this.rValueLabel.TabIndex = 12;
@@ -167,7 +171,7 @@
             // gValueLabel
             // 
             this.gValueLabel.AutoSize = true;
-            this.gValueLabel.Location = new System.Drawing.Point(639, 123);
+            this.gValueLabel.Location = new System.Drawing.Point(643, 227);
             this.gValueLabel.Name = "gValueLabel";
             this.gValueLabel.Size = new System.Drawing.Size(15, 13);
             this.gValueLabel.TabIndex = 13;
@@ -176,7 +180,7 @@
             // bValueLabel
             // 
             this.bValueLabel.AutoSize = true;
-            this.bValueLabel.Location = new System.Drawing.Point(721, 123);
+            this.bValueLabel.Location = new System.Drawing.Point(725, 227);
             this.bValueLabel.Name = "bValueLabel";
             this.bValueLabel.Size = new System.Drawing.Size(14, 13);
             this.bValueLabel.TabIndex = 14;
@@ -184,7 +188,7 @@
             // 
             // brightnessTextBox
             // 
-            this.brightnessTextBox.Location = new System.Drawing.Point(540, 218);
+            this.brightnessTextBox.Location = new System.Drawing.Point(544, 47);
             this.brightnessTextBox.Name = "brightnessTextBox";
             this.brightnessTextBox.Size = new System.Drawing.Size(79, 20);
             this.brightnessTextBox.TabIndex = 15;
@@ -192,7 +196,7 @@
             // brightnessLabel
             // 
             this.brightnessLabel.AutoSize = true;
-            this.brightnessLabel.Location = new System.Drawing.Point(461, 225);
+            this.brightnessLabel.Location = new System.Drawing.Point(465, 54);
             this.brightnessLabel.Name = "brightnessLabel";
             this.brightnessLabel.Size = new System.Drawing.Size(59, 13);
             this.brightnessLabel.TabIndex = 16;
@@ -200,7 +204,7 @@
             // 
             // changeBrightnessButton
             // 
-            this.changeBrightnessButton.Location = new System.Drawing.Point(646, 205);
+            this.changeBrightnessButton.Location = new System.Drawing.Point(650, 34);
             this.changeBrightnessButton.Name = "changeBrightnessButton";
             this.changeBrightnessButton.Size = new System.Drawing.Size(103, 45);
             this.changeBrightnessButton.TabIndex = 17;
@@ -210,7 +214,7 @@
             // 
             // resultPictureBox
             // 
-            this.resultPictureBox.Location = new System.Drawing.Point(102, 246);
+            this.resultPictureBox.Location = new System.Drawing.Point(33, 221);
             this.resultPictureBox.Name = "resultPictureBox";
             this.resultPictureBox.Size = new System.Drawing.Size(224, 156);
             this.resultPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -220,7 +224,7 @@
             // originalPictureLabel
             // 
             this.originalPictureLabel.AutoSize = true;
-            this.originalPictureLabel.Location = new System.Drawing.Point(187, 20);
+            this.originalPictureLabel.Location = new System.Drawing.Point(115, 18);
             this.originalPictureLabel.Name = "originalPictureLabel";
             this.originalPictureLabel.Size = new System.Drawing.Size(63, 13);
             this.originalPictureLabel.TabIndex = 19;
@@ -229,7 +233,7 @@
             // resultPictureLabel
             // 
             this.resultPictureLabel.AutoSize = true;
-            this.resultPictureLabel.Location = new System.Drawing.Point(187, 218);
+            this.resultPictureLabel.Location = new System.Drawing.Point(115, 205);
             this.resultPictureLabel.Name = "resultPictureLabel";
             this.resultPictureLabel.Size = new System.Drawing.Size(70, 13);
             this.resultPictureLabel.TabIndex = 20;
@@ -237,7 +241,7 @@
             // 
             // invertPictureButton
             // 
-            this.invertPictureButton.Location = new System.Drawing.Point(519, 357);
+            this.invertPictureButton.Location = new System.Drawing.Point(317, 123);
             this.invertPictureButton.Name = "invertPictureButton";
             this.invertPictureButton.Size = new System.Drawing.Size(100, 45);
             this.invertPictureButton.TabIndex = 21;
@@ -247,7 +251,7 @@
             // 
             // greyscaleAveragingButton
             // 
-            this.greyscaleAveragingButton.Location = new System.Drawing.Point(649, 281);
+            this.greyscaleAveragingButton.Location = new System.Drawing.Point(317, 57);
             this.greyscaleAveragingButton.Name = "greyscaleAveragingButton";
             this.greyscaleAveragingButton.Size = new System.Drawing.Size(100, 45);
             this.greyscaleAveragingButton.TabIndex = 22;
@@ -255,11 +259,50 @@
             this.greyscaleAveragingButton.UseVisualStyleBackColor = true;
             this.greyscaleAveragingButton.Click += new System.EventHandler(this.greyscaleAveragingButton_Click);
             // 
+            // processLabel
+            // 
+            this.processLabel.AutoSize = true;
+            this.processLabel.Location = new System.Drawing.Point(341, 18);
+            this.processLabel.Name = "processLabel";
+            this.processLabel.Size = new System.Drawing.Size(53, 13);
+            this.processLabel.TabIndex = 23;
+            this.processLabel.Text = "Olah Citra";
+            // 
+            // thresholdLabel
+            // 
+            this.thresholdLabel.AutoSize = true;
+            this.thresholdLabel.Location = new System.Drawing.Point(465, 111);
+            this.thresholdLabel.Name = "thresholdLabel";
+            this.thresholdLabel.Size = new System.Drawing.Size(68, 13);
+            this.thresholdLabel.TabIndex = 24;
+            this.thresholdLabel.Text = "Thresholding";
+            // 
+            // thresholdValueTextBox
+            // 
+            this.thresholdValueTextBox.Location = new System.Drawing.Point(544, 108);
+            this.thresholdValueTextBox.Name = "thresholdValueTextBox";
+            this.thresholdValueTextBox.Size = new System.Drawing.Size(79, 20);
+            this.thresholdValueTextBox.TabIndex = 25;
+            // 
+            // thresholdPictureButton
+            // 
+            this.thresholdPictureButton.Location = new System.Drawing.Point(650, 95);
+            this.thresholdPictureButton.Name = "thresholdPictureButton";
+            this.thresholdPictureButton.Size = new System.Drawing.Size(103, 45);
+            this.thresholdPictureButton.TabIndex = 26;
+            this.thresholdPictureButton.Text = "Threshold Gambar";
+            this.thresholdPictureButton.UseVisualStyleBackColor = true;
+            this.thresholdPictureButton.Click += new System.EventHandler(this.thresholdPictureButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.thresholdPictureButton);
+            this.Controls.Add(this.thresholdValueTextBox);
+            this.Controls.Add(this.thresholdLabel);
+            this.Controls.Add(this.processLabel);
             this.Controls.Add(this.greyscaleAveragingButton);
             this.Controls.Add(this.invertPictureButton);
             this.Controls.Add(this.resultPictureLabel);
@@ -317,6 +360,10 @@
         private System.Windows.Forms.Label resultPictureLabel;
         private System.Windows.Forms.Button invertPictureButton;
         private System.Windows.Forms.Button greyscaleAveragingButton;
+        private System.Windows.Forms.Label processLabel;
+        private System.Windows.Forms.Label thresholdLabel;
+        private System.Windows.Forms.TextBox thresholdValueTextBox;
+        private System.Windows.Forms.Button thresholdPictureButton;
     }
 }
 
