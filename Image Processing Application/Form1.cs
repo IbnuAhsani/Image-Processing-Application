c486106cf5e0f2ac5b1a4bc12d4d7ab41d6f7c20
Ibnu Ahsani <ibnu.muhari@gmail.com>
2019-03-11 17:54:27 +0700

    add logic to calculate chessboard, manhattan, and euclidean distance

--- Image Processing Application/Form1.cs
+++ Image Processing Application/Form1.cs
@@ -24,12 +24,11 @@ namespace Image_Processing_Application
         int mouseClickCount = 0;
 
         // Variables for the Bitmap's row and column
-        int row, column;
+        int x1, y1, x2, y2;
 
         // Bitmap variables for original and result pictures
         Bitmap bitMapOriginal, bitMapResult;
-
-
+        
         /**
          * Function to change the picture in the main picture box
          */
@@ -48,15 +47,67 @@ namespace Image_Processing_Application
             }
         }
 
-
         private void euclideanDistanceButton_Click(object sender, EventArgs e)
         {
+            bool pointOneCoordinateNullOrEmpty = pointOneXCoordinateTextBox.Text == "" ||
+                    pointOneYCoordinateTextBox.Text == "";
+
+            bool pointTwoCoordinateNullOrEmpty = pointTwoXCoordinateTextBox.Text == "" ||
+                    pointTwoYCoordinateTextBox.Text == "";
+            
+            if (!(pointOneCoordinateNullOrEmpty && pointTwoCoordinateNullOrEmpty))
+            {
+                double chessboardDistance = Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+                double manhattanDistance = Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+                double euclideanDistance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+                double roundedChessboardDistance = Math.Round(chessboardDistance, 3);
+                double roundedManhattanDistance = Math.Round(manhattanDistance, 3);
+                double roundedEuclideanDistance = Math.Round(euclideanDistance, 3);
+
+                chessboardDistanceValueTextBox.Text = roundedChessboardDistance.ToString();
+                manhattanDistanceValueTextBox.Text = roundedManhattanDistance.ToString();
+                euclideanDistanceValueTextBox.Text = roundedEuclideanDistance.ToString();
+            } else
+            {
+                var errorMessage = new Form
+                {
+                    ShowInTaskbar = false,
+                    TopMost = true,
+                    Size = new Size(300, 300),
+                    Location = new Point(300, 200),
+                    StartPosition = FormStartPosition.Manual,
+                    Text = "Salah satu koordinat belum dipilih",
+                };
+
+                // Display the form
+                errorMessage.Show();
+            }
 
         }
 
 
         private void mainPictureBox_MouseClick(object sender, MouseEventArgs e)
         {
+            if (mouseClickCount == 0)
+            {
+                pointOneXCoordinateTextBox.Text = e.X.ToString();
+                pointOneYCoordinateTextBox.Text = e.Y.ToString();
+
+                x1 = e.X;
+                y1 = e.Y;
+
+                mouseClickCount++;
+            } else
+            {
+                pointTwoXCoordinateTextBox.Text = e.X.ToString();
+                pointTwoYCoordinateTextBox.Text = e.Y.ToString();
+
+                x2 = e.X;
+                y2 = e.Y;
+
+                mouseClickCount = 0;
+            } 
 
         }
     }
